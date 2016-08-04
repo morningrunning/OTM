73d3f6636c630139a89c80d193e15ea1b50e1a6a
“morningrunning” <rmorgan01@gmail.com>
2016-08-04 19:17:04 -0400

    Committee

---
+++ OTMConstants.swift
@@ -0,0 +1,9 @@
+//
+//  OTMConstants.swift
+//  OTM
+//
+//  Created by Ronald Morgan on 8/4/16.
+//  Copyright © 2016 Ronald Morgan. All rights reserved.
+//
+
+import Foundation
---
+++ StudentData.swift
@@ -0,0 +1,9 @@
+//
+//  StudentData.swift
+//  OTM
+//
+//  Created by Ronald Morgan on 8/4/16.
+//  Copyright © 2016 Ronald Morgan. All rights reserved.
+//
+
+import Foundation
